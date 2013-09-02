c5f6401ed633d2f079836b38452255fe891f2c46
Mark Bucciarelli <mkbucc@gmail.com>
2013-09-02 16:23:48 -0400

    Extract ACS 3.3 sources under webroot.

---
+++ ansible/acs_get.sh
@@ -0,0 +1,29 @@
+#! /bin/sh -e
+# Package up the ACS tarball.
+# Created: Mon Sep  2 15:31:23 EDT 2013
+
+VERS=$(grep ACS_VERSION acs_vars.yml | cut -d ':' -f 2 | tr -d ' ')
+
+# We want stuff to exand to acs subdir.
+DEST=acs
+
+DIRS="
+../bin
+../parameters
+../tcl
+../templates
+../www
+"
+
+FILES="
+../readme.txt
+"
+
+if [ ! -f acs-${VERS}.tgz ]; then 
+    rm -rf $DEST
+    mkdir $DEST
+    for d in $DIRS ; do cp -Rp $d $DEST; done
+    for f in $FILES; do cp -Rp $d $DEST; done
+    tar czvf acs-${VERS}.tgz ./acs
+    rm -rf $DEST
+fi
