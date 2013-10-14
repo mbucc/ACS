810d2c532c9aacd4b695e8ac13e321d64eccdcb5
Mark Bucciarelli <mkbucc@gmail.com>
2013-10-14 12:29:53 -0400

    Zap commented-out code.

--- ansible/stale_vars.sh
+++ ansible/stale_vars.sh
@@ -3,11 +3,6 @@
 # List unused yml variables.
 
 for f in *_vars.yml ; do
-#  if [ "$f" = "test.yml"  ] ; then continue ; fi
-#  if [ "$f" = "$PLAYBOOK" ] ; then continue ; fi
-#  if ! grep $f $PLAYBOOK > /dev/null; then
-#    echo $f
-#  fi
   for v in $(grep "^[A-Z ]\+" $f | cut -d : -f 1); do
     if grep $v --exclude $f *.yml > /dev/null ; then continue ; fi
     if grep $v              *.j2  > /dev/null ; then continue ; fi
