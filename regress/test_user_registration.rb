7075d0182d2f000515d8e8d8975aaf573adf82df
Mark Bucciarelli <mkbucc@gmail.com>
2013-10-14 14:21:36 -0400

    Test that a new user can register.
    Note: I need to re-work the backup/restore approach to resetting data after a
    unit test.  Oracle's restore is smarter than I thought; it goes back to the
    last backup and then re-applies all transactions that occurred after that
    backup.  Pretty slick, but totally doesn't work for setup/teardown.  I'll need
    to use a flashback recovery point.

--- regress/test_user_registration.rb
+++ regress/test_user_registration.rb
@@ -5,21 +5,27 @@ require "watir-webdriver"
 browser = Watir::Browser.new :ff
 browser.goto "192.168.30.117:8000/pvt/home"
 
-browser.text_field(:name => 'email').set 'a0@t.com'
+browser.text_field(:name => 'email').set 'a@b.com'
 browser.text_field(:name => 'password').set 'abc'
+
 f = browser.form(:name, "login")
 f.submit
 
 browser.text_field(:name => 'password_confirmation').set 'abc'
 browser.text_field(:name => 'first_names').set 'fname'
 browser.text_field(:name => 'last_name').set 'lname'
+browser.text_field(:name => 'url').set 'http://www.google.com'
 
 f1 = browser.form(:action, "user-new-2")
 f1.submit
 
-# browser.text.include? 'fname lname's workspace at Network'
+if browser.text.include? "fname lname's workspace at Network"
+    puts "User account created . . . . . . . PASS"
+else
+    puts "User account created . . . . . . . FAIL"
+end
 
-puts browser.url
-puts browser.title
+#puts browser.url
+#puts browser.title
 
-browser.close
+#browser.close
