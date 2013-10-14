9f7769b72329be9ba991399a92de07c1ea0439c2
Mark Bucciarelli <mkbucc@gmail.com>
2013-10-14 01:19:29 -0400

    Put Oracle in ARCHIVELOG mode.
    Also, double Flash Recovery Area to 20G.

--- regress/register_user.rb
+++ regress/register_user.rb
@@ -5,14 +5,14 @@ require "watir-webdriver"
 browser = Watir::Browser.new :ff
 browser.goto "192.168.30.117:8000/pvt/home"
 
-browser.text_field(:name => 'email').set 'a@t.com'
+browser.text_field(:name => 'email').set 'a0@t.com'
 browser.text_field(:name => 'password').set 'abc'
 f = browser.form(:name, "login")
 f.submit
 
 browser.text_field(:name => 'password_confirmation').set 'abc'
 browser.text_field(:name => 'first_names').set 'fname'
-browser.text_field(:name => 'last_name').set 'lname'=> ""
+browser.text_field(:name => 'last_name').set 'lname'
 
 f1 = browser.form(:action, "user-new-2")
 f1.submit
