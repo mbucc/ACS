f99a0e8d73155168e940fbd1fc779f6b138dda87
Mark Bucciarelli <mkbucc@gmail.com>
2013-10-20 14:39:39 -0400

    Fix portrait messaging when uploads are not open.

--- www/pvt/portrait/index.tcl
+++ www/pvt/portrait/index.tcl
@@ -20,9 +20,39 @@ if { ![db_0or1row user_check {
     return
 }
 
+#         approved_p		general_portraits      result
+#         ---------- 		------------------    ----------------------
+#    1        't'   		no record at all       error message
+#    2        'f'   		no record at all       error message
+#
+#    3        't'   		approved_p = 'f'       error message
+#    4        'f'   		approved_p = 'f'       waiting approval message
+#
+#    5        't'   		approved_p = 't'       OK
+#    6        'f'   		approved_p = 't'       OK
+
+set approved_p [util_decode [ad_parameter DefaultUploadApprovalPolicy "general-portraits"] "open" "t" "f"]
+
+
+# Cases 1 and 2: no portrait record at all.
+if { ![db_0or1row portrait_check {
+   select portrait_id,
+	  portrait_upload_date,
+	  portrait_comment,
+	  portrait_original_width,
+	  portrait_original_height,
+	  portrait_client_file_name
+     from general_portraits
+    where on_what_id = :user_id
+      and on_which_table = 'USERS'
+      and portrait_primary_p = 't'
+}] } {
+    ad_return_complaint 1 "<li>You shouldn't have gotten here; we don't have a portrait on file for you."
+    return
+}
 
 if { ![db_0or1row portrait_check {
-   select portrait_id, 
+   select portrait_id,
 	  portrait_upload_date,
 	  portrait_comment,
 	  portrait_original_width,
@@ -34,10 +64,52 @@ if { ![db_0or1row portrait_check {
       and approved_p = 't'
       and portrait_primary_p = 't'
 }] } {
-    ad_return_complaint 1 "<li>You shouldn't have gotten here; we don't have a portrait on file for you."
+    # Case 3: Uploads are open, but for some reason portrait is not approved.
+    # Case 4: Uploads are not open and portrait is not approved.
+    doc_return  200 text/html "
+    [ad_header "Portrait of $first_names $last_name"]
+
+    <h2>Portrait of $first_names $last_name</h2>
+
+    [ad_context_bar_ws "Your Portrait"]
+
+    <hr>
+
+    <p>
+    You won't be able to see your portrait until the site admin approves it (we had some users abuse the privilege of
+    approval-free uploads).
+    </p>
+
+    <br>
+
+    Data:
+
+    <ul>
+    <li>Uploaded:  [util_AnsiDatetoPrettyDate $portrait_upload_date]
+    <li>Original Name:  $portrait_client_file_name
+    <li>Comment: [util_decode $portrait_comment "" "" "
+    <blockquote>
+    $portrait_comment
+    </blockquote>"]
+
+    </ul>
+
+    Options:
+
+    <ul>
+    <li><a href=comment-edit>edit your comment</a>
+    <li><a href=\"upload\">upload a replacement</a>
+
+    <p>
+
+    <li><a href=\"erase\">erase</a>
+
+    </ul>
+
+    [ad_footer]
+    "
     return
 }
-
 if { ![empty_string_p $portrait_original_width] && ![empty_string_p $portrait_original_height] } {
     set widthheight "width=$portrait_original_width height=$portrait_original_height"
 } else {
