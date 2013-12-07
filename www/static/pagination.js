1fc2361f1fb64b76340fa8402935efa39d5e9ab6
Mark Bucciarelli <mkbucc@gmail.com>
2013-12-07 17:53:52 -0500

    Add reveal styles before calculating heights.
      * add debug switch that logs element heights.
      * improve a couple variable names.

--- www/static/pagination.js
+++ www/static/pagination.js
@@ -5,6 +5,8 @@
 
 var paginate = (function () {
 
+	var _debug = true;
+
 	// Return true if the element is a heading.
 	function is_heading(el) {
 		return el && /H[1-6]/.test(el.nodeName);
@@ -66,31 +68,69 @@ var paginate = (function () {
 
 		var el = page.lastChild;
 
+		if (_debug)
+			console.log("*** " + page.offsetHeight +
+				": entering trim_page()");
+
+
 		insert(buffer, el);
 
 		// Always move headings to the next page.
-		if (is_heading(el))
+		if (is_heading(el)) {
+			if (_debug)
+				console.log(page.offsetHeight +
+					": after trimming off " + el.nodeName);
 			return;
+		}
 
 		if (is_blockquote(el)) {
 			var bq = document.createElement('blockquote');
 			page.appendChild(bq);
 			add_until_full(page, bq, el, canvas_px);
-			insert(el, bq.lastChild);
+			if (bq.lastChild) {
+				if (_debug)
+					console.log("removing " + bq.lastChild.nodeName
+							+ ", too big");
+				insert(el, bq.lastChild);
+			}
+			else {
+				// Couldn't add a single <p> without
+				// overflowing, so remove block quote.
+				page.removeChild(bq);
+				if (_debug)
+					console.log("Couldn't add a single <p>.");
+			}
+			if (_debug)
+				console.log("*** " + page.offsetHeight +
+						": exiting trim_page()");
 			return;
 		}
 
 		insert(buffer, el);
 
+		if (_debug)
+			console.log(page.offsetHeight +
+					": after trimming off " + el.nodeName);
+
 	};
 
-	function add_until_full(page, parent_el, buf, canvas_px) {
+	function add_until_full(page, parent_el, buf, is_full_px) {
+		var
+			slop_percentage = 5
+			, factor = (1 - slop_percentage/100.)
+			;
 		for (
 				var el = buf.firstChild;
-				el && page.offsetHeight < canvas_px;
+				el && page.offsetHeight < (is_full_px*factor);
 				el = buf.firstChild
 				)
+		{
 			parent_el.appendChild(el);
+			if (_debug) {
+				console.log(page.offsetHeight +
+					": " + el.nodeName);
+			}
+		}
 	}
 
 
@@ -102,7 +142,9 @@ var paginate = (function () {
 			, page_n = 0
 			, buffer = document.createElement('div')
 			, page = null
-			, loop_limit = 100	// max # of slides
+			, max_pages_conceivable = 100
+			, reveal_center = false
+			, reveal_transition = 'linear'
 			;
 
 		// Move page contents from <body> to a buffer.
@@ -110,7 +152,9 @@ var paginate = (function () {
 			buffer.appendChild(el);
 
 		var reveal = document.createElement('div');
-		reveal.className = "reveal";
+		reveal.className = "reveal " +
+			reveal_transition +
+			(reveal_center ? " center" : "") ;
 		b.appendChild(reveal);
 
 		var slides = document.createElement('div');
@@ -118,9 +162,13 @@ var paginate = (function () {
 		reveal.appendChild(slides);
 
 		console.log("canvas_px = " + canvas_px);
-		while (buffer.childNodes.length > 0 && page_n < loop_limit) {
+		while (buffer.childNodes.length > 0
+				&& page_n < max_pages_conceivable)
+		{
 			page = document.createElement('section');
 			page_n += 1;
+			page.className = (page_n == 1 ? "present" : "future");
+
 			slides.appendChild(page);
 
 			add_until_full(page, page, buffer, canvas_px);
@@ -131,20 +179,23 @@ var paginate = (function () {
 			console.log("page " + page_n + ": " + page.offsetHeight);
 		}
 
-		if (page_n >= loop_limit)
-			// XXX: Post error to the server.
+		if (page_n >=  max_pages_conceivable)
+			// XXX: Post this error back to the server.
 			;
 
 		// Full list of configuration options available here:
 		// https://github.com/hakimel/reveal.js#configuration
 		Reveal.initialize({
 			controls: true,
-			progress: true,
+			progress: false,
 			history: true,
-			center: true,
+			center: reveal_center,
 
-			theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-			transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+			// available themes are in /css/theme
+			theme: Reveal.getQueryHash().theme,
+
+			// default/cube/page/concave/zoom/linear/fade/none
+			transition: Reveal.getQueryHash().transition || reveal_transition,
 
 			// Parallax scrolling
 			// parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
