0443938bb80ed4a12557857f1ffeda6ec2ec2f73
Mark Bucciarelli <mkbucc@gmail.com>
2013-12-07 20:01:44 -0500

    Merge and minify css as well.

--- minify.sh
+++ minify.sh
@@ -4,9 +4,11 @@
 # Merge static files, put MD5 in filename, and update properties file.
 
 DIR=www/static
-TMPFN=tmp.js
 
+# JavaScript
+TMPFN=tmp.js
 files="
+reveal.js
 pagination.js
 acs.js
 "
@@ -24,9 +26,33 @@ cp "$PFN" "$PFN.$(date +%F_%H-%M-%S)"
 cat "$PFN" | sed "s;acs\(\-[^.]\+\)\?\.js;$FN;" > mkb.tcl
 mv mkb.tcl "$PFN"
 
-if grep $FN $PFN > /dev/null ; then
-        echo $0 succeeded.
-else
+if ! grep $FN $PFN > /dev/null ; then
+        echo $0 ERROR, $FN not in $PFN >&2
+        exit 1
+fi
+
+# CSS
+TMPFN=tmp.css
+files="
+reveal.css
+serif.css
+acs.css
+"
+
+rm -f $TMPFN
+for f in $files; do
+	cat $DIR/$f >> $TMPFN
+done
+
+FN=acs-$(md5sum $TMPFN | awk '{print $1}').css
+
+cp $TMPFN $DIR/$FN
+PFN=$(find parameters -name "*.tcl" | xargs grep -l "acs\(\-[^.]\+\)\?\.css")
+cp "$PFN" "$PFN.$(date +%F_%H-%M-%S)"
+cat "$PFN" | sed "s;acs\(\-[^.]\+\)\?\.css;$FN;" > mkb.tcl
+mv mkb.tcl "$PFN"
+
+if ! grep $FN $PFN > /dev/null ; then
         echo $0 ERROR, $FN not in $PFN >&2
         exit 1
 fi
