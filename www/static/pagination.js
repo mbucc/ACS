762b2f195fc4a067395dd8c66c8dd1dc3913265c
Mark Bucciarelli <mkbucc@gmail.com>
2013-12-08 21:07:59 -0500

    Smarter pagination.
      * split any element with children.
      * split paragraphs on words.

--- www/static/pagination.js
+++ www/static/pagination.js
@@ -5,7 +5,11 @@
 
 var paginate = (function () {
 
-	var _debug = true;
+	var
+		_debug = true
+		, page_full_slop_percentage = 5
+		, slop_factor = (1 - page_full_slop_percentage/100.)
+		;
 
 	// Return true if the element is a heading.
 	function is_heading(el) {
@@ -14,16 +18,22 @@ var paginate = (function () {
 
 	// Return true if the element is a header tag.
 	function is_header(el) {
-		return el && el.nodeName == "HEADER";
+		return el && el.nodeName.toUpperCase() == "HEADER";
 	};
 
 	function is_break(el) {
-		return el && el.nodeName == "BR";
+		return el && el.nodeName.toUpperCase() == "BR";
 	};
 
 	function is_blockquote(el) {
-		return el && el.nodeName == "BLOCKQUOTE";
+		return el && el.nodeName.toUpperCase() == "BLOCKQUOTE";
 	};
+
+	function is_paragraph(el) {
+		return el && el.nodeName.toUpperCase() == "P";
+	};
+
+
 
 	// Returns the first heading element inside the <header> section.
 	function getHeader(elname) {
@@ -60,76 +70,126 @@ var paginate = (function () {
 
 	// Insert element as first child in parent.
 	function insert(parent_el, el) {
+
 		parent_el.insertBefore(el, parent_el.firstChild);
+
 	}
 
-	// Page is overfull.  Split last element added.
-	function trim_page(buffer, page, canvas_px) {
+	// Paragraphs can be long and they don't have child elements.  So we
+	// just keep adding words until the page is full.
+	function add_words_until_full(page, new_paragraph, paragraph, is_full_px) {
 
-		var el = page.lastChild;
-
-		if (_debug)
-			console.log("*** " + page.offsetHeight +
-				": entering trim_page()");
-
-
-		insert(buffer, el);
-
-		// Always move headings to the next page.
-		if (is_heading(el)) {
-			if (_debug)
-				console.log(page.offsetHeight +
-					": after trimming off " + el.nodeName);
-			return;
-		}
-
-		if (is_blockquote(el)) {
-			var bq = document.createElement('blockquote');
-			page.appendChild(bq);
-			add_until_full(page, bq, el, canvas_px);
-			if (bq.lastChild) {
-				if (_debug)
-					console.log("removing " + bq.lastChild.nodeName
-							+ ", too big");
-				insert(el, bq.lastChild);
-			}
-			else {
-				// Couldn't add a single <p> without
-				// overflowing, so remove block quote.
-				page.removeChild(bq);
-				if (_debug)
-					console.log("Couldn't add a single <p>.");
-			}
-			if (_debug)
-				console.log("*** " + page.offsetHeight +
-						": exiting trim_page()");
-			return;
-		}
-
-		insert(buffer, el);
-
-		if (_debug)
-			console.log(page.offsetHeight +
-					": after trimming off " + el.nodeName);
-
-	};
-
-	function add_until_full(page, parent_el, buf, is_full_px) {
 		var
-			slop_percentage = 5
-			, factor = (1 - slop_percentage/100.)
+			words = paragraph
+				.innerHTML
+				.replace(/  */g, ' ')
+				.split(' ')
+				.reverse()
+			, word
 			;
+
+		if (_debug)
+			console.log("------ add_words_until_full: " +
+				       	words.length + " words, is_full_px = " +
+					is_full_px);
+
+		paragraph.innerHTML = '';
+
+		while ((word = words.pop())) {
+
+			new_paragraph.innerHTML += ' ' + word;
+
+			if (page.offsetHeight >= is_full_px)
+				break;
+
+			if (_debug)
+				console.log("    " + page.offsetHeight + ": " +
+						word + " (" +
+						(page.offsetHeight >= is_full_px) +
+						")"
+						);
+		}
+
+		if (page.offsetHeight > is_full_px) {
+
+			words.push(word);
+
+			var s = new_paragraph.innerHTML;
+
+			var i = s.lastIndexOf(" ");
+
+			new_paragraph.innerHTML = s.substring(0, i);
+
+		}
+
+		words.reverse();
+
+		paragraph.innerHTML = words.join(' ');
+
+	}
+
+	function add_until_full(page, parent_el, el_pool, is_full_px) {
+
+		if (_debug)
+			console.log("------ add_until_full: parent = " +
+				       	parent_el.nodeName);
+
 		for (
-				var el = buf.firstChild;
-				el && page.offsetHeight < (is_full_px*factor);
-				el = buf.firstChild
+				var el = el_pool.firstChild;
+				el && page.offsetHeight < is_full_px;
+				el = el_pool.firstChild
 				)
 		{
+
 			parent_el.appendChild(el);
-			if (_debug) {
-				console.log(page.offsetHeight +
-					": " + el.nodeName);
+
+			if (_debug)
+				console.log(page.offsetHeight + ": " + el.nodeName);
+
+		}
+
+		if (page.offsetHeight >= is_full_px) {
+
+			var el = parent_el.lastChild;
+
+			if (!el) {
+
+				// We could not add any children to the new
+				// parent node, so remove the new parent node.
+
+				var grandparent = parent_el.parentElement;
+
+				grandparent.removeChild(parent_el);
+
+				return;
 			}
+
+			insert(el_pool, el);
+
+
+			if (is_paragraph(el)) {
+
+				var new_parent = document.createElement("P");
+
+				parent_el.appendChild(new_parent);
+
+				add_words_until_full(page, new_parent, el, is_full_px);
+
+			}
+
+			else if (el.childNodes.length > 0) {
+
+				var new_parent = document.createElement(el.nodeName);
+
+				parent_el.appendChild(new_parent);
+
+				add_until_full(page, new_parent, el, is_full_px);
+
+			}
+
+			else
+				; // leave element off page.
+
 		}
 	}
 
@@ -171,10 +231,7 @@ var paginate = (function () {
 
 			slides.appendChild(page);
 
-			add_until_full(page, page, buffer, canvas_px);
-
-			if (page.offsetHeight > canvas_px)
-				trim_page(buffer, page, canvas_px);
+			add_until_full(page, page, buffer, canvas_px * slop_factor);
 
 			console.log("page " + page_n + ": " + page.offsetHeight);
 		}
